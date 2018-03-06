1936603a7639689bc41ab627a9cc91613f47b715
Liam Sobczak <liam@gotpop.net>
2018-03-06 18:27:36 +0000

    Added basic component

---
+++ src/components/CallApplyBind.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="call">
+    <h1>Call, Apply, Bind</h1>
+    <p>Call runs a function on an object of your choice. It accepts arguments.</p>
+    <p>Apply is the same as call except it can accept an array.</p>
+    <p>Bind creates but does not call the function.</p>
+    <hr />
+    <p>Call changes context.</p>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class CallApplyBind extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
